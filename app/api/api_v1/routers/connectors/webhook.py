b5dfc03e9507e679daeb17ffe9e914c3d1c36cad
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-05-31 03:48:07 -0500

    fix router connector parameters

--- app/api/api_v1/routers/connectors/webhook.py
+++ app/api/api_v1/routers/connectors/webhook.py
@@ -5,7 +5,7 @@ from app.core.config import get_settings, Settings
 router = APIRouter()
 
 def get_webhook_connector(settings: Settings = Depends(get_settings)) -> WebhookConnector:
-    return WebhookConnector(settings.webhook_secret)
+    return WebhookConnector(webhook_url=settings.webhook_secret)
 
 @router.post("/webhooks/webhook")
 async def process_webhook_update(request: Request, webhook_connector: WebhookConnector = Depends(get_webhook_connector)):
