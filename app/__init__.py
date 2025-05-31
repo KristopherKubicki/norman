58c8223eaf44a3853b2203a5369c3a071fc56eef
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-05-31 03:11:05 -0500

    fix forwardref patch for python 3.12

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,20 @@
 
 from typing import ForwardRef, Any, cast
 import inspect
+import sys
 import pydantic.typing as _pydantic_typing
 
 # Pydantic 1.x doesn't support Python 3.12's updated ForwardRef._evaluate
 sig = inspect.signature(ForwardRef._evaluate)
-if 'recursive_guard' in sig.parameters:
+py_ver = sys.version_info
+if py_ver >= (3, 12) and 'recursive_guard' in sig.parameters:
     def _evaluate_forwardref(type_: ForwardRef, globalns: Any, localns: Any) -> Any:
         return cast(Any, type_)._evaluate(globalns, localns, None, recursive_guard=set())
+
+    _pydantic_typing.evaluate_forwardref = _evaluate_forwardref
+elif py_ver < (3, 12) and 'recursive_guard' not in sig.parameters:
+    def _evaluate_forwardref(type_: ForwardRef, globalns: Any, localns: Any) -> Any:
+        return cast(Any, type_)._evaluate(globalns, localns, None)
 
     _pydantic_typing.evaluate_forwardref = _evaluate_forwardref
 
